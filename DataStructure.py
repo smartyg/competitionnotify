99b7aa133c57b678e255748e9abafd1f5ff9ac58
Martijn Goedhart <goedhart.martijn@gmail.com>
2025-02-15 20:50:29 +0100

    more changes

--- DataStructure.py
+++ DataStructure.py
@@ -4,6 +4,10 @@ import typing
 import numpy
 import pandas
 import re
+import logging
+import traceback
+
+logger = logging.getLogger(__name__)
 
 homeVenueCodes = ["ALK", "AMS"]
 categoryCode = ["HPF", "DPF", "HPE", "DPE", "HPD", "DPD", "HPC", "DPC", "HPB", "DPB", "HPA", "DPA", "HC1", "DC1", "HC2", "DC2", "HB1", "DB1", "HB2", "DB2", "HA1", "DA1", "HA2", "DA2", "HN1", "DN1", "HN2", "DN2", "HN3", "DN3", "HN4", "DN4", "HSA", "DSA", "HSB", "DSB", "H40", "D40", "H45", "D45", "H50", "D50", "H55", "D55", "H60", "D60", "H65", "D65", "H70", "D70", "H75", "D75", "H80", "D80"]
@@ -71,6 +75,12 @@ class BaseDataStructure:
 			self._table.loc[len(self._table)] = new_entry
 			return True
 		except ValueError:
+			logger.info ("table: " + str(self._table))
+			logger.info ("dtypes: " + str(self._table.dtypes))
+			logger.warning ("Error can not insert \"" + str(new_entry) + "\": " + traceback.format_exc())
+			return False
+		except NotImplementedError:
+			logger.warning ("Error can not insert \"" + str(new_entry) + "\": " + traceback.format_exc())
 			return False
 
 	def get(self) -> pandas.DataFrame:
@@ -85,9 +95,19 @@ class BaseDataStructure:
 
 	def _recode(self, items: list) -> list:
 		ret = list()
-		for i in items:
-			if isinstance(i, str):
-				ret.append(i.encode())
+		keys = list(self._layout.keys())
+		for n,i in enumerate(list(items)):
+		#for i in items:
+			if len(self._layout[keys[n]]) >= 2:
+				if self._layout[keys[n]][1] == 'S':
+					if isinstance(i, str):
+						ret.append(i.encode())
+					elif i is None:
+						ret.append(b'')
+					else:
+						ret.append(i)
+				else:
+					ret.append(i)
 			else:
 				ret.append(i)
 		return ret
@@ -106,6 +126,9 @@ class HomeVenues(BaseDataStructure):
 		}
 		super().__init__(file, layout, 9)
 
+	def _selector(self, code: str) -> pandas.Series:
+		return (self._table['code']==code.encode())
+
 	def _updateHelper(self, selector, names: list[str], address):
 		for name in names:
 			if address[name] is not None:
@@ -118,11 +141,39 @@ class HomeVenues(BaseDataStructure):
 		print("  code: " + str(code))
 		print("  name: " + str(name))
 		if self.columnHasValue('code', code.encode()):
-			selector = self._table['code']==code.encode()
+			selector = self._selector(code)
 			self._updateHelper(selector, ['line1', 'line2', 'stateOrProvince', 'postalCode', 'city', 'countryCode'], address)
 		else:
 			self.append([code, address['countryCode'], name, address['city'], address['line1'], address['line2'], address['postalCode'], address['stateOrProvince']])
 		return None
+
+	def getVenue (self, code: str|None) -> dict:
+		if code is None:
+			return {
+				"code": None,
+				"countryCode": None,
+				"name": None,
+				"city": None,
+				"line1": None,
+				"line2": None,
+				"postalCode": None,
+				"stateOrProvince": None
+		   }
+		selector = self._selector(code)
+		select = self._table.loc[selector]
+		if (len(select)) == 0:
+			return {
+				"code": None,
+				"countryCode": None,
+				"name": None,
+				"city": None,
+				"line1": None,
+				"line2": None,
+				"postalCode": None,
+				"stateOrProvince": None
+		   }
+		else:
+			return select.iloc[0,:].to_dict()
 
 class ProcessedCompetitions(BaseDataStructure):
 	def __init__(self, file: str):
@@ -137,7 +188,7 @@ class Emails(BaseDataStructure):
 		layout = {
 			"email_id": "|S16",
 			"comp_id": "|S16",
-			"email": "|S1024",
+			"email": "|S4096",
 			"send": "?",
 			"to": "|S1024"
 		}
@@ -166,7 +217,7 @@ class Skaters(BaseDataStructure):
 		}
 		super().__init__(file, layout, 9)
 
-	def filter(self, venue: str, homeVenueFilter: str|list|None = None, categoryFilter: str|list|None = None, clubCodeFilter: str|list|None = None, disciplineCodeFilter: str|None = None, invitees: list|None = None) -> list:
+	def filter(self, venue: str|None, homeVenueFilter: str|list|None = None, categoryFilter: str|list|None = None, clubCodeFilter: str|list|None = None, disciplineCodeFilter: str|None = None, invitees: list|None = None) -> list:
 		print("Skaters.filter()")
 		print("  venue: " + str(venue))
 		print("  homeVenueFilter: " + str(homeVenueFilter))
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import asyncio
 import random
 import jinja2
 from DataStructure import ProcessedCompetitions, Emails, Skaters, EmailsSkaters, HomeVenues
+import minify_html
+import traceback
 
 logger = logging.getLogger(__name__)
 
@@ -61,9 +63,8 @@ async def download_competitions():
 			async with aiohttp.ClientSession() as session:
 				async with session.get('https://inschrijven.schaatsen.nl/api/competitions') as response:
 					logger.debug ("New competition file downloaded")
-					content = await response.text()
-					#print(content)
-					competitions = json.loads(content)
+					competitions = json.loads(await response.text())
+
 					# We have a new competition file, now cancel all outstanding tasks
 					await kill_competitions_tasks ()
 
@@ -73,6 +74,9 @@ async def download_competitions():
 			await asyncio.sleep(24*3600)
 	except asyncio.CancelledError:
 		logger.warning ("Request to kill the main loop")
+		await kill_competitions_tasks ()
+	except Exception as e:
+		logger.error ("Error: " + traceback.format_exc())
 		await kill_competitions_tasks ()
 
 	finally:
@@ -86,7 +90,7 @@ def start_process_competition(id: uuid, code: str|None, when: datetime|int) -> a
 	name = "Task " + str(id)
 	if code is not None:
 		if len(code) > 0:
-			name += " (" + name + ")"
+			name += " (" + code + ")"
 	task = asyncio.create_task(process_competition (id, when))
 	task.set_name(name)
 	task.add_done_callback(competitionsTasksToProcess.discard)
@@ -94,26 +98,22 @@ def start_process_competition(id: uuid, code: str|None, when: datetime|int) -> a
 	return task
 
 async def process_all_competitions(competitions: list):
-	logger.debug ("Process new competion file")
+	logger.debug ("Process new competition file")
 	now = datetime.now(timezone.utc)
 
 	for comp in competitions:
+		if comp['test']:
+			continue
 		comp_settings = comp['settings']
 		comp_id = uuid.UUID(comp['id'], version=4)
 
 		if not comp_settings['isClosed']:
 			if not processed_competitions.columnHasValue('comp_id', comp_id.bytes):
 				start_process_competition(comp_id, comp['code'], -1)
-				# task = asyncio.create_task(process_competition (comp_id, -1))
-				# task.add_done_callback(competitionsTasksToProcess.discard)
-				# competitionsTasksToProcess.add(task)
 		else:
 			time_open = datetime.fromisoformat(comp_settings['opens'])
 			if time_open > now:
 				start_process_competition(comp_id, comp['code'], time_open)
-				# task = asyncio.create_task(process_competition (comp_id, time_open))
-				# task.add_done_callback(competitionsTasksToProcess.discard)
-				# competitionsTasksToProcess.add(task)
 
 		# if comp['venue'] is not None:
 		# 	venues.update(address=comp['venue']['address'], code=comp['venue']['code'], name=comp['venue']['name'])
@@ -122,12 +122,12 @@ async def process_all_competitions(competitions: list):
 		# give also some time to other tasks
 		await asyncio.sleep (0)
 
-def find_dict(d, key, value):
+def find_dict(d: dict, key: str, value: str) -> dict:
 	for e in d:
 		if key in e:
 			if e[key] == value:
 				return e
-	return None
+	return {}
 
 def get_urls(id: uuid) -> dict[str: str]:
 	urls = {
@@ -139,6 +139,7 @@ def get_urls(id: uuid) -> dict[str: str]:
 
 def get_links(id: uuid) -> dict[str: str]:
 	links = {
+		'general': 'https://inschrijven.schaatsen.nl/',
 		'subscription': 'https://inschrijven.schaatsen.nl/#/wedstrijd/' + str(id) + '/inschrijven',
 		'information': 'https://inschrijven.schaatsen.nl/#/wedstrijd/' + str(id) + '/informatie',
 		'participants': 'https://inschrijven.schaatsen.nl/#/wedstrijd/' + str(id) + '/deelnemers'
@@ -150,7 +151,7 @@ async def process_competition(id: uuid, when: datetime|int = 0):
 	try:
 		if isinstance(when, int):
 			if when == -1:
-				when = random.randint(1,60)
+				when = random.randint(1, 60)
 			logger.debug ("(" + str(id) + "): wait for " + str(when) + " seconds to start processing competition")
 			await asyncio.sleep(when)
 		else:
@@ -160,7 +161,7 @@ async def process_competition(id: uuid, when: datetime|int = 0):
 				logger.debug ("(" + str(id) + "): wait for " + str(wait) + " seconds to start processing competition")
 				await asyncio.sleep(wait)
 	except asyncio.CancelledError:
-		logger.debug ("(" + str(id) + "): task got cancelled before it could start")
+		logger.info ("(" + str(id) + "): task got cancelled before it could start")
 		# end the task
 		return None
 
@@ -179,27 +180,38 @@ async def process_competition(id: uuid, when: datetime|int = 0):
 				async with session.get(url) as response:
 					data[key] = json.loads(await response.text())
 	except asyncio.CancelledError:
+		logger.info ("(" + str(id) + "): task got cancelled during download of competition files")
 		# end the task
 		return None
 
-	#test = []
+	if (data['competitions'] is None) or (data['distancecombinations'] is None) or (data['settings'] is None):
+		logger.error ("(" + str(id) + "): download of competition files failed")
+		return None
+	else:
+		logger.debug ("(" + str(id) + "): download of competition files complete")
+
 	skaters = []
 	distances = []
-	email_html = None
+	email_html = str()
+	venue = None
+	serie = None
 
 	try:
 		if data['competitions']['settings']['isClosed']:
 			logger.warning ("(" + str(id) + "): wedstrijd is (nog) niet open")
 			return False
 
+		if data['competitions']['venue'] is not None:
+			venue = data['competitions']['venue']['code']
+
 		for d1 in data['settings']:
 			d2 = find_dict (data['distancecombinations'], 'id', d1['distanceCombinationId'])
-			#d = d1 | d2
-			#test.append (d1 | d2)
-			skaters_distance = all_skaters.filter(venue=data['competitions']['venue']['code'], homeVenueFilter=d1['homeVenueFilter'], categoryFilter=d2['categoryFilter'], clubCodeFilter=d1['clubCodeFilter'], invitees=d1['invitees'], disciplineCodeFilter=None)
+			skaters_distance = all_skaters.filter(venue=venue, homeVenueFilter=d1['homeVenueFilter'], categoryFilter=d2['categoryFilter'], clubCodeFilter=d1['clubCodeFilter'], invitees=d1['invitees'], disciplineCodeFilter=None)
+
 			for skater in skaters_distance:
 				if skater not in skaters:
 					skaters.append (skater)
+
 			distance_entry = {}
 			distance_entry['id'] =  uuid.UUID(d1['distanceCombinationId'], version=4)
 			distance_entry['name'] = d2['name']
@@ -207,10 +219,9 @@ async def process_competition(id: uuid, when: datetime|int = 0):
 			distance_entry['cost'] = {'competition': d1['competitionPaymentOption'], 'serie': d1['seriePaymentOption']}
 			distance_entry['distances'] = []
 			for d in d2['distances']:
-				distance_entry['distances'].append (distance['value'])
+				distance_entry['distances'].append (d['value'])
 
 			distances.append (distance_entry)
-
 
 
 				# limitTimeDistanceDiscipline
@@ -222,38 +233,78 @@ async def process_competition(id: uuid, when: datetime|int = 0):
 				# 13=selectie active leden
 				# 4=selectie
 
+		if data['competitions']['serie'] is not None:
+			if data['competitions']['serie']['id'] is not None:
+				serie_id = uuid.UUID(data['competitions']['serie']['id'], version=4)
+				serie = find_others_in_serie (serie_id, id)
 
-		# Serializing json
-		#json_object = json.dumps(test, indent=2)
+		logger.info ("(" + str(id) + "): distances: " + str(len(distances)) + "; skaters: " + str(len(skaters)))
 
-		# Writing to sample.json
-		# with open(str(id) + ".json", "w") as outfile:
-		# 	outfile.write(json_object)
-
-		email_html = await compose_html_mail (id, get_links(id), data['competitions'], distances)
+		email_html = await compose_html_mail (id, get_links(id), data['competitions'], distances, venues.getVenue(venue), serie=serie)
 		await asyncio.sleep(0)
 	except asyncio.CancelledError:
+		logger.info ("(" + str(id) + "): task got cancelled during processing")
 		asyncio.current_task().uncancel()
+	except Exception as e:
+		logger.error ("(" + str(id) + "/" + d1['distanceCombinationId'] + "): " + traceback.format_exc())
 
 	finally:
-		if email_html is not None:
+		if email_html != '':
 			email_id = enqueue_email (id, skaters, email_html)
 			if email_id is not None:
 				add_processed_competition(id, email_id)
 		return None
 
-async def compose_html_mail (id: uuid, links: dict[str: str], competition: dict, distances: list) -> str:
-	email = await email_template.render_async({
-		'id': str(id),
-		'links': links,
-		'competition': competition,
-		'distances': distances
-	})
-	filename = "./" + str(id) + ".email"
-	logger.debug ("(" + str(id) + "): email generated and stored in: " + str(filename))
-	with open(filename, "w") as outfile:
-		outfile.write(email)
-	return email
+def find_others_in_serie (serie_id: uuid, exclude_id: "uuid|None" = None) -> list|None:
+	return None
+
+async def compose_html_mail (id: uuid, links: dict[str: str], competition: dict, combinations: list, venue: dict|None = None, serie: list|None = None) -> str:
+	email = str()
+	try:
+		logger.debug ("(" + str(id) + "): run template Jinja2 generator")
+		email_full = await email_template.render_async({
+			'id': str(id),
+			'code': competition['code'],
+			'discipline': competition['discipline'],
+			'starts': datetime.fromisoformat(competition['starts']),
+			'ends': datetime.fromisoformat(competition['ends']),
+			'name': competition['name'],
+			'closes': datetime.fromisoformat(competition['settings']['closes']),
+			'url': competition['settings']['contact']['url'],
+			'opens': competition['settings']['opens'],
+			'withdrawUntil': datetime.fromisoformat(competition['settings']['withdrawUntil']),
+			'message1': competition['settings']['extra'],
+			'message2': competition['extra'],
+			'location_description': competition['location'],
+			'links': links,
+			'combinations': combinations,
+			'venue': venue,
+			'serie': serie
+		})
+		email=email_full
+		# email = minify_html.minify(email_full,
+		# 					 minify_js=True,
+		# 					 minify_css=True,
+		# 					 keep_input_type_text_attr=True,
+		# 					 do_not_minify_doctype=True,
+		# 					 ensure_spec_compliant_unquoted_attribute_values=True,
+		# 					 keep_closing_tags=True,
+		# 					 keep_html_and_head_opening_tags=True,
+		# 					 keep_spaces_between_attributes=True,
+		# 					 keep_comments=False,
+		# 					 remove_processing_instructions=True)
+		filename = "./" + str(id) + ".email"
+		logger.debug ("(" + str(id) + "): email generated and stored in: " + str(filename))
+		with open(filename, "w") as outfile:
+			outfile.write(email)
+
+	except asyncio.CancelledError as e:
+		raise e
+	except Exception as e:
+		logger.error ("Error: " + traceback.format_exc())
+
+	finally:
+		return email
 
 def enqueue_email (comp_id: uuid, skaters: list, email_html: str) -> uuid:
 	email_uuid = uuid.uuid4()
